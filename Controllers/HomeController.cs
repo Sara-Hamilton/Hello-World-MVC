a94274bf42f0dc8d74ffec6a5497f3c5dc516265
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2018-02-04 16:11:49 -0800

    add form to get and display names in letter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,5 +31,20 @@ namespace FriendLetter.Controllers
       myLetterVariable.SetSender("John");
       return View(myLetterVariable);
     }
+
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+
+    [Route("/greeting_card")]
+    public ActionResult GreetingCard()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(Request.Query["recipient"]);
+      myLetterVariable.SetSender(Request.Query["sender"]);
+      return View("Hello", myLetterVariable);
+    }
   }
 }
